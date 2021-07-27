ae98618e27a5542a5d28f08f6913bd265b174391
Amila Ferron <aferron@pdx.edu>
2021-07-26 23:35:00 -0700

    Complete Bayes algorithm with ~81% accuracy

--- new_toy.py
+++ new_toy.py
@@ -16,11 +16,11 @@ min_std_dev = 0.0001
 start = time.perf_counter()
 
 # import the data to a numpy array
-# raw_data = np.loadtxt("spambase.data", dtype='float', delimiter=',')
-raw_data = np.array([(1,2,3,0),(2,2,2,0),(3,3,3,1),(1,1,1,1),(3,4,5,0),(3,3,3,0)])
+raw_data = np.loadtxt("spambase.data", dtype='float', delimiter=',')
+# raw_data = np.array([(1,2,3,0),(2,2,2,0),(3,3,3,1),(1,1,1,1),(3,4,5,0),(3,3,3,0)])
 
 # shuffle the data
-# raw_data = np.random.permutation(raw_data)
+raw_data = np.random.permutation(raw_data)
 
 
 
@@ -28,8 +28,8 @@ raw_data = np.array([(1,2,3,0),(2,2,2,0),(3,3,3,1),(1,1,1,1),(3,4,5,0),(3,3,3,0)
 data = np.split(raw_data, [int(len(raw_data) / 2)])
 train_data = data[0]
 test_data = data[1]
-print("test data\n", test_data)
-print("train data\n", train_data)
+# print("test data\n", test_data)
+# print("train data\n", train_data)
 len_train = len(data[0])
 len_test = len(data[1])
 attributes = len(data[0][0])
@@ -116,7 +116,7 @@ else:
 # train_real_data is then [~1400 x 57]
 train_spam_data = np.delete(train_spam_data, attributes - 1, 1)
 train_real_data = np.delete(train_real_data, attributes - 1, 1)
-print("train spam data shape: ", np.shape(train_spam_data), " train_real_data shape: ", np.shape(train_real_data))
+# print("train spam data shape: ", np.shape(train_spam_data), " train_real_data shape: ", np.shape(train_real_data))
 print("Delete last element of each row:", end='\t\t\t')
 if len(train_spam_data[0]) != attributes - 1 or len(train_real_data[0]) != attributes - 1:
     print("FAILED")
@@ -155,8 +155,8 @@ if train_real_means[index] != sum / size:
     quit()
 else:
     print("PASSED")
-print("train spam means: ", train_spam_means)
-print("train real means: ", train_real_means)
+# print("train spam means: ", train_spam_means)
+# print("train real means: ", train_real_means)
 
 
 
@@ -206,71 +206,96 @@ elif np.min(train_spam_std) < min_std_dev or np.min(train_real_std) < min_std_de
     quit()
 else:
     print("PASSED")
-    range = np.ptp(train_spam_std, axis=0)
-    print("Range of standard deviations: ")
-    print("spam: ", range)
-    range = np.ptp(train_real_std)
-    print("real: ", range)
-    range = np.ptp(train_spam_data)
-    print("Range of data: ")
-    print("spam: ", range)
-    range = np.ptp(train_real_data)
-    print("real: ", range)
-    print("Minimums: ")
-    print("spam: ", np.min(train_spam_data))
-    print("real: ", np.min(train_real_data))
-    print("Maximums: ")
-    print("spam: ", np.max(train_spam_data))
-    print("real: ", np.max(train_real_data))
-    print("standard deviations: ")
-    print("spam:")
-    print(train_spam_std)
-    print("real:")
-    print(train_real_std)
-
-
-# subtract the mean from the data point
-real_diff = np.subtract(test_data, train_real_means)
-print("\n test data:\n", test_data, "\n minus\ntrain real means:\n", train_real_means)
-print("test data - real mean: \n", real_diff)
-
-# square the difference to get the numerator for the exponent
-numerator = np.power(real_diff, 2)
-print("numerator: \n", numerator)
-
-# get the denominator
-denominator = 2 * np.power(train_real_std, 2)
-print("denominator: \n", denominator)
-
-# get the exponent
-exponent = -1 * np.divide(numerator, denominator)
-print("exponent: \n", exponent)
-
-# raise e to the exponent
-exponential = np.exp(exponent)
-print("exponential: \n", exponential)
-
-# divide the exponential by sqrt(2 pi) * standard deviation
-denominator2 = math.sqrt(2 * np.pi) * train_real_std
-p_input_0 = np.divide(exponential, denominator2) 
-print("denominator 2: ", denominator2)
-print("real values: \n", p_input_0)
-
-# take the log of all the values
-logs = np.log(p_input_0) 
-print("logs: \n", logs)
-
-# get the probability that it's real
-sum = np.sum(logs, axis=1)
-p_real = math.log(1 - train_p_1) + sum
-print("sum P(xi|0): ", sum)
-print("log(P(0)): ", math.log(1 - train_p_1))
-print("probability that it's real: ", p_real)
+    # ptp_range = np.ptp(train_spam_std, axis=0)
+    # print("Range of standard deviations: ")
+    # print("spam: ", ptp_range)
+    # ptp_range = np.ptp(train_real_std)
+    # print("real: ", ptp_range)
+    # ptp_range = np.ptp(train_spam_data)
+    # print("Range of data: ")
+    # print("spam: ", ptp_range)
+    # ptp_range = np.ptp(train_real_data)
+    # print("real: ", ptp_range)
+    # print("Minimums: ")
+    # print("spam: ", np.min(train_spam_data))
+    # print("real: ", np.min(train_real_data))
+    # print("Maximums: ")
+    # print("spam: ", np.max(train_spam_data))
+    # print("real: ", np.max(train_real_data))
+    # print("standard deviations: ")
+    # print("spam:")
+    # print(train_spam_std)
+    # print("real:")
+    # print(train_real_std)
 
 
 
 
+# collect real and spam arrays into a single array for standard deviation and means
+train_mean = np.array([train_real_means])
+train_mean = np.append(train_mean, [train_spam_means], axis=0)
+train_std = np.array([train_real_std])
+train_std = np.append(train_std, [train_spam_std], axis=0)
+# print(train_mean)
 
+
+
+for i in range(2):
+    # subtract the mean from the data point
+    diff = np.subtract(test_data, train_mean[i])
+    # print("\n test data:\n", test_data, "\n minus\ntrain real means:\n", train_real_means)
+    # print("test data - real mean: \n", real_diff)
+
+    # square the difference to get the numerator for the exponent
+    numerator = np.power(diff, 2)
+    # print("numerator: \n", numerator)
+
+    # get the denominator
+    denominator = 2 * np.power(train_std[i], 2)
+    # print("denominator: \n", denominator)
+
+    # get the exponent
+    exponent = -1 * np.divide(numerator, denominator)
+    # print("exponent: \n", exponent)
+
+    # raise e to the exponent
+    exponential = np.exp(exponent)
+    # print("exponential: \n", exponential)
+
+    # divide the exponential by sqrt(2 pi) * standard deviation
+    denominator2 = math.sqrt(2 * np.pi) * train_std[i]
+
+    # probability of getting that data point given that it's a 0 or 1, per i
+    probability = np.divide(exponential, denominator2) 
+    # print("denominator 2: ", denominator2)
+    # print("real values: \n", p_input_0)
+
+    # take the log of all the values
+    logs = np.log(probability) 
+    # print("logs: \n", logs)
+
+    # get the probability that it's real
+    sum = np.sum(logs, axis=1)
+    if i == 0:
+        p_i = np.array([math.log(1 - train_p_1) + sum])
+    else:
+        p_i = np.append(p_i, np.array([math.log(1 - train_p_1) + sum]), axis=0)
+    # print("sum P(xi|0): ", sum)
+    # print("log(P(0)): ", math.log(1 - train_p_1))
+    # print("probability that it's a ", i , ": ", p_i)
+    print("shape: ", np.shape(p_i) )
+
+
+category = np.argmax(p_i, axis=0)
+print("category shape: ", np.shape(category))
+accuracy_check = test_labels - category
+
+accuracy = len_train - np.count_nonzero(accuracy_check)
+
+print(accuracy)
+print(len_train)
+
+print("accuracy: ", accuracy / len_train)
 
 
 print("Time elapsed: ", time.perf_counter() - start)
